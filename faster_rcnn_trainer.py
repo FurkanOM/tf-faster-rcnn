delta calculation bug fixed

faster_rcnn_trainer.py
@@ -61,17 +61,17 @@ padding_values = (tf.constant(0, tf.uint8), tf.constant(-1, tf.float32), tf.cons
 VOC_train_data = VOC_train_data.padded_batch(batch_size, padded_shapes=padded_shapes, padding_values=padding_values)
 VOC_val_data = VOC_val_data.padded_batch(batch_size, padded_shapes=padded_shapes, padding_values=padding_values)
 
-rpn_train_feed = faster_rcnn.generator(VOC_train_data, hyper_params, preprocess_input)
-rpn_val_feed = faster_rcnn.generator(VOC_val_data, hyper_params, preprocess_input)
+frcnn_train_feed = faster_rcnn.generator(VOC_train_data, hyper_params, preprocess_input)
+frcnn_val_feed = faster_rcnn.generator(VOC_val_data, hyper_params, preprocess_input)
 
 model_checkpoint = ModelCheckpoint(frcnn_model_path, save_best_only=True, save_weights_only=True, monitor="val_loss", mode="auto")
 early_stopping = EarlyStopping(monitor="val_loss", patience=20, verbose=0, mode="auto")
 
 step_size_train = VOC_train_data_len // batch_size
 step_size_val = VOC_val_data_len // batch_size
-frcnn_model.fit(rpn_train_feed,
+frcnn_model.fit(frcnn_train_feed,
                 steps_per_epoch=step_size_train,
-                validation_data=rpn_val_feed,
+                validation_data=frcnn_val_feed,
                 validation_steps=step_size_val,
                 epochs=epochs,
                 callbacks=[early_stopping, model_checkpoint])

helpers.py
@@ -66,8 +66,8 @@ def get_deltas_from_bboxes(bboxes, gt_boxes):
     gt_ctr_x = gt_boxes[:, :, 1] + 0.5 * gt_width
     gt_ctr_y = gt_boxes[:, :, 0] + 0.5 * gt_height
     #
-    bbox_width = tf.where(tf.equal(bbox_width, 0), tf.ones_like(bbox_width), bbox_width)
-    bbox_height = tf.where(tf.equal(bbox_height, 0), tf.ones_like(bbox_height), bbox_height)
+    bbox_width = tf.where(tf.equal(bbox_width, 0), 1e-5, bbox_width)
+    bbox_height = tf.where(tf.equal(bbox_height, 0), 1e-5, bbox_height)
     delta_x = tf.where(tf.equal(gt_width, 0), tf.zeros_like(bbox_width), tf.truediv((gt_ctr_x - bbox_ctr_x), bbox_width))
     delta_y = tf.where(tf.equal(gt_height, 0), tf.zeros_like(bbox_height), tf.truediv((gt_ctr_y - bbox_ctr_y), bbox_height))
     delta_w = tf.where(tf.equal(gt_width, 0), tf.zeros_like(bbox_width), tf.math.log(gt_width / bbox_width))